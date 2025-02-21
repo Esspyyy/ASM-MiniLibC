61818e122d3afd63d312ae35471d6c890f400e62
Spencer <pay.spencer@gmail.com>
2025-02-21 15:44:07 +0100

    [add] index.asm

--- index.asm
+++ index.asm
@@ -7,30 +7,8 @@
 
 section .text
     global index
+    extern strchr
 
 ; The following code is identical to strchr
 index:
-    ; Prologue
-    push rbp            ; Save frame pointer
-    mov rbp, rsp       ; Create new frame
-
-loop_start:
-    cmp byte [rdi], sil    ; Compare current char with target
-    je found               ; Jump if found
-    cmp byte [rdi], 0      ; Check for string end
-    je not_found           ; Jump if end reached
-    inc rdi                ; Move to next character
-    jmp loop_start
-
-found:
-    mov rax, rdi           ; Return pointer to found character
-    jmp end
-
-not_found:
-    xor rax, rax           ; Return NULL
-
-end:
-    ; Epilogue
-    mov rsp, rbp          ; Restore stack pointer
-    pop rbp               ; Restore frame pointer
-    ret
+    jmp strchr    ; Direct jump to strchr implementation
