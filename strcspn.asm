93db5806e3f7c60004ac3858a9814537164d4736
Spencer <pay.spencer@gmail.com>
2025-02-21 14:03:40 +0100

    [add] comments

--- strcspn.asm
+++ strcspn.asm
@@ -9,20 +9,23 @@ section .text
     global strcspn
 
 strcspn:
+    ; Prologue
     push rbp
     mov rbp, rsp
-    xor rax, rax
+    push rcx           ; Save rcx since we modify it
+    push rdx           ; Save rdx since we modify it (dl)
+    xor rax, rax       ; Initialize counter to 0
 
 outer_loop:
-    cmp byte [rdi + rax], 0
+    cmp byte [rdi + rax], 0    ; Check end of string1
     je end
-    mov rcx, rsi
+    mov rcx, rsi              ; Reset rcx to start of string2
 
 inner_loop:
-    cmp byte [rcx], 0
+    cmp byte [rcx], 0         ; Check end of string2
     je next_char
-    mov dl, byte [rcx]
-    cmp byte [rdi + rax], dl
+    mov dl, byte [rcx]        ; Load character from string2
+    cmp byte [rdi + rax], dl  ; Compare with current char of string1
     je end
     inc rcx
     jmp inner_loop
@@ -32,6 +35,9 @@ next_char:
     jmp outer_loop
 
 end:
+    ; Epilogue
+    pop rdx
+    pop rcx
     mov rsp, rbp
     pop rbp
     ret
