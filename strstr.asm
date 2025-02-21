1312398a9b624bfb80440b68b06a6f32a2df6ab7
Spencer <pay.spencer@gmail.com>
2025-02-21 14:27:07 +0100

    [add] comments

--- strstr.asm
+++ strstr.asm
@@ -9,47 +9,56 @@ section .text
     global strstr
 
 strstr:
+    ; Prologue
     push rbp
     mov rbp, rsp
-    mov r8, rdi
-    cmp byte [rsi], 0
+    push rcx           ; Push used registers onto the stack
+    push rdx
+    push r8
+
+    mov r8, rdi        ; Save original string pointer into r8
+    cmp byte [rsi], 0  ; Check if needle is empty
     je empty_needle
 
 outer_loop:
-    mov rdi, r8
-    mov rcx, rsi
-    cmp byte [rdi], 0
+    mov rdi, r8        ; Reset search position
+    mov rcx, rsi       ; Reset needle pointer
+    cmp byte [rdi], 0  ; Check if we reached end of haystack
     je not_found
 
 inner_loop:
-    mov al, byte [rcx]
-    cmp al, 0
+    mov al, byte [rcx] ; Load char from needle
+    cmp al, 0          ; Check if end of needle
     je found
-    mov dl, byte [rdi]
-    cmp dl, 0
+    mov dl, byte [rdi] ; Load char from haystack
+    cmp dl, 0          ; Check if end of haystack
     je not_found
-    cmp al, dl
+    cmp al, dl         ; Compare characters
     jne next_char
-    inc rdi
+    inc rdi            ; Move to next char
     inc rcx
     jmp inner_loop
 
 next_char:
-    inc r8
+    inc r8             ; Move to next potential match position
     jmp outer_loop
 
 empty_needle:
-    mov rax, r8
+    mov rax, r8        ; Return original string if needle is empty
     jmp end
 
 found:
-    mov rax, r8
+    mov rax, r8        ; Return pointer to match
     jmp end
 
 not_found:
-    xor rax, rax
+    xor rax, rax       ; Return NULL if not found
 
 end:
+    ; Epilogue
+    pop r8             ; Restore registers in reverse order
+    pop rdx
+    pop rcx
     mov rsp, rbp
     pop rbp
     ret
