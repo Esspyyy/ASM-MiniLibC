c5d4425abc0927247d928eb60fa080e4d4ea2e6f
Spencer <pay.spencer@gmail.com>
2025-02-21 13:17:40 +0100

    [fix] strncmp.asm

--- strncmp.asm
+++ strncmp.asm
@@ -9,31 +9,48 @@ section .text
     global strncmp
 
 strncmp:
+    ; Prologue
     push rbp
     mov rbp, rsp
-    cmp rdx, 0
-    je equal
+    push rcx        ; Save rcx since we'll modify it
 
-compare_loop:
-    mov al, byte [rdi]
-    mov cl, byte [rsi]
+    ; if n == 0, return 0
+    test rdx, rdx
+    jz return_zero
+
+loop:
+    ; if n == 0, strings are equal up to n
+    test rdx, rdx
+    jz return_zero
+
+    ; compare current characters
+    movzx rax, byte [rdi]
+    movzx rcx, byte [rsi]
+
+    ; if characters differ, return difference
     cmp al, cl
-    jne not_equal
+    jne return_diff
+
+    ; if we hit a null terminator, strings are equal
+    test al, al
+    jz return_zero
+
+    ; move to next character
     inc rdi
     inc rsi
     dec rdx
-    jnz compare_loop
+    jmp loop
 
-equal:
-    xor rax, rax
+return_diff:
+    sub rax, rcx
     jmp end
 
-not_equal:
-    movzx rax, al
-    movzx rcx, cl
-    sub rax, rcx
+return_zero:
+    xor rax, rax
 
 end:
+    ; Epilogue
+    pop rcx         ; Restore rcx
     mov rsp, rbp
     pop rbp
     ret
