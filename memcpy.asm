711e94a5cbf7bff89dc1b2b017fa3ad5e19bdaf3
Spencer <pay.spencer@gmail.com>
2025-02-21 13:31:33 +0100

    [add] comments and prologue epilogue

--- memcpy.asm
+++ memcpy.asm
@@ -9,21 +9,36 @@ section .text
     global memcpy
 
 memcpy:
+    ; Prologue: Save the base pointer and the rcx register
     push rbp
     mov rbp, rsp
+    push rcx
+
+    ; Set rax to the destination pointer (rdi)
     mov rax, rdi
+
+    ; If the length (rdx) is 0, jump to the end
     cmp rdx, 0
     je end
 
 copy_loop:
+    ; Copy byte from source (rsi) to destination (rdi)
     mov cl, byte [rsi]
     mov byte [rdi], cl
+
+    ; Increment source and destination pointers
     inc rsi
     inc rdi
+
+    ; Decrement the length counter
     dec rdx
+
+    ; If length counter is not zero, repeat the loop
     jnz copy_loop
 
 end:
+    ; Epilogue
+    pop rcx         ; Restore rcx
     mov rsp, rbp
     pop rbp
     ret
