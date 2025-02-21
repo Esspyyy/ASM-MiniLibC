9561c7c0b42ca28139983a37bb9868496ce653e7
Spencer <pay.spencer@gmail.com>
2025-02-21 13:46:28 +0100

    [add] comments

--- memset.asm
+++ memset.asm
@@ -9,22 +9,29 @@ section .text
     global memset
 
 memset:
+    ; Prologue
     push rbp
     mov rbp, rsp
+
+    ; Initialize rax with the destination address
     mov rax, rdi
+
+    ; Check if the length is zero
     cmp rdx, 0
     je end
 
 fill_loop:
+    ; Fill the memory with the specified value
     mov byte [rax], sil
     inc rax
     dec rdx
     jnz fill_loop
 
 end:
-    mov rax, rdi
+    ; Epilogue
     mov rsp, rbp
     pop rbp
+
+    ; Return
+    mov rax, rdi
     ret
-
-
