5d18ce7f7d561af295c775567fcd5b3aa723476d
Spencer <pay.spencer@gmail.com>
2025-02-21 14:01:27 +0100

    [add] comments

--- strcmp.asm
+++ strcmp.asm
@@ -9,30 +9,42 @@ section .text
     global strcmp
 
 strcmp:
-    push rbp
+    ; Prologue
+    push rbp            ; Save frame pointer
     mov rbp, rsp
+    push rcx           ; Save rcx since we modify it
 
 compare_loop:
+    ; Load char from both strings
     mov al, byte [rdi]
     mov cl, byte [rsi]
+
+
+    ; Jump to not_equal if characters are different
     cmp al, cl
     jne not_equal
+
+    ; Check for end of string and leave loop if so
     cmp al, 0
     je equal
+
+    ; Move to next chars and reloop
     inc rdi
     inc rsi
     jmp compare_loop
 
 not_equal:
-    movzx rax, al
+    movzx rax, al         ; Zero-extend chars for comparison
     movzx rcx, cl
-    sub rax, rcx
+    sub rax, rcx          ; Calculate difference
     jmp end
 
 equal:
-    xor rax, rax
+    xor rax, rax          ; Return 0 for equal strings
 
 end:
+    ; Epilogue
+    pop rcx
     mov rsp, rbp
     pop rbp
     ret
