9a238d5260e81b49e59d157aef39888302af910a
Spencer <pay.spencer@gmail.com>
2025-02-21 14:12:56 +0100

    [add] comments

--- strrchr.asm
+++ strrchr.asm
@@ -9,27 +9,30 @@ section .text
     global strrchr
 
 strrchr:
+    ; Prologue
     push rbp
     mov rbp, rsp
-    xor rax, rax
+
+    xor rax, rax       ; Initialize return value to NULL
 
 loop_start:
-    cmp byte [rdi], 0
-    je check_null
-    cmp byte [rdi], sil
-    jne next_char
-    mov rax, rdi
+    cmp byte [rdi], 0      ; Check for end of string
+    je check_null          ; If null terminator, check if we're looking for null
+    cmp byte [rdi], sil    ; Compare current char with target
+    jne next_char          ; If no match, continue
+    mov rax, rdi           ; If match, save current position
 
 next_char:
-    inc rdi
+    inc rdi                ; Move to next character
     jmp loop_start
 
 check_null:
-    cmp byte [rdi], sil
-    jne end
-    mov rax, rdi
+    cmp byte [rdi], sil    ; Check if we're looking for null terminator
+    jne end                ; If not, we're done
+    mov rax, rdi           ; If yes, save position of null terminator
 
 end:
+    ; Epilogue
     mov rsp, rbp
     pop rbp
     ret
